6786ad09381680958c8604198e97966462aaa058
MinisterZ <1289411876@qq.com>
2020-03-19 10:28:53 +0800

    第一次

---
+++ src/main/java/com/cuit/config/MySpringMvcConfigurer.java
@@ -0,0 +1,35 @@
+package com.cuit.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @version JDK  1.8.151
+ * @Author: Mirrors
+ * @Description: Chengdu City
+ */
+
+@Configuration
+public class MySpringMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+         return    new WebMvcConfigurer(){
+                //添加视图控制
+                @Override                                                   //  找/main/login.html
+                public void addViewControllers(ViewControllerRegistry registry) {
+                    registry.addViewController("/").setViewName("main/index");
+                    registry.addViewController("/index.html").setViewName("main/login");
+                    registry.addViewController("/main.html").setViewName("main/index");
+
+                }
+         };
+    }
+
+
+}
