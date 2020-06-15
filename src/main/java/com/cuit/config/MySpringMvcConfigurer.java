ba4296626daec374ec7ae2120851036d366a5e1b
MinisterZ <1289411876@qq.com>
2020-06-15 14:03:30 +0800

    第二次

--- src/main/java/com/cuit/config/MySpringMvcConfigurer.java
+++ src/main/java/com/cuit/config/MySpringMvcConfigurer.java
@@ -3,8 +3,6 @@ package com.cuit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,7 +22,7 @@ public class MySpringMvcConfigurer {
                 @Override                                                   //  找/main/login.html
                 public void addViewControllers(ViewControllerRegistry registry) {
                     registry.addViewController("/").setViewName("main/index");
-                    registry.addViewController("/a.html").setViewName("a");
+                    registry.addViewController("/zxy.html").setViewName("main/zxy");
 
 
                 }
